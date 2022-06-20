2d116fd4fd9e334d6fbe8562e641e27ec9959064
Gannon Smith <gansmith@umich.edu>
2022-06-20 00:57:25 -0400

    implement dest_connection

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,5 +1,6 @@
 use std::collections::HashMap;
 use std::{env, thread};
+use std::borrow::Cow;
 use std::io::prelude::*;
 use std::net::{Shutdown, TcpListener, TcpStream};
 
@@ -29,9 +30,9 @@ fn main() {
 }
 
 // conventionally uses port 1080
-fn server_listener(address: &str) {
-    let listener = TcpListener::bing(address).expect("Binding failed...");
-    println!("Server listening on {}", address);
+fn server_listener(server_address: &str) {
+    let listener = TcpListener::bing(server_address).expect("Binding failed...");
+    println!("Server listening on {}", server_address);
 
     for stream in listener.incoming() {
         match stream {
@@ -48,21 +49,32 @@ fn server_listener(address: &str) {
     }
 }
 
-fn handle_client_connection(mut stream: TcpStream) {
+fn handle_client_connection(mut client_stream: TcpStream) {
     let mut buffer = [0; 1024];
-    stream.write("Please enter [dest_addr]\n".as_bytes()).unwrap();
+    client_stream.write("Please enter [dest_addr]\n".as_bytes()).unwrap();
 
-    match stream.read(&mut buffer) {
+    match client_stream.read(&mut buffer) {
         Ok(_) => {
             let dest_addr = String::from_utf8_lossy(&buffer[..]);
+            dest_connection(dest_addr);
         },
         Err(_) => {
-            println!("An error occured, terminating connection with {}", stream.peer_addr().unwrap());
+            println!("An error occured, terminating connection with {}", client_stream.peer_addr().unwrap());
             stream.shutdown(Shutdown::Both).expect("Shutdown failed...");
         }
     } {}
 }
 
-fn dest_connection(dest_addr: &str) {
+fn dest_connection(dest_addr: Cow<str>) {
+    if let Ok(dest_stream) = TcpStream::connect(dest_addr) {
+        println!("Connected to dest addr {}", dest_addr);
+        handle_dest_connection(dest_stream);
+    } else {
+        println!("Couldn't connect to dest addr {}", dest_addr);
+    }
+}
+
+fn handle_dest_connection(mut dest_stream: TcpStream) {
+    let mut buffer = [0; 1024];
 
 }
