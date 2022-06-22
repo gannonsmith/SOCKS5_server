b3ede0378d44ecb4d3a15c81bf44bb58c014b0d2
Gannon Smith <gansmith@umich.edu>
2022-06-21 23:26:16 -0400

    add design for RFC

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashMap;
 use std::{env, thread};
 use std::borrow::Cow;
 use std::io::prelude::*;
+use std::io::Split;
 use std::net::{Shutdown, TcpListener, TcpStream};
 
 struct Server {
@@ -31,7 +32,7 @@ fn main() {
 
 // conventionally uses port 1080
 fn server_listener(server_address: &str) {
-    let listener = TcpListener::bing(server_address).expect("Binding failed...");
+    let listener = TcpListener::bind(server_address).expect("Binding failed...");
     println!("Server listening on {}", server_address);
 
     for stream in listener.incoming() {
@@ -50,36 +51,43 @@ fn server_listener(server_address: &str) {
 }
 
 fn handle_client_connection(mut client_stream: TcpStream) {
+    println!("[*] Received connection request from {:?}", client_stream);
+
     let mut buffer = [0; 1024];
-    client_stream.write("Please enter [dest_addr]\n".as_bytes()).unwrap();
+    client_stream.write("Please enter [dest]\n".as_bytes()).unwrap();
 
     match client_stream.read(&mut buffer) {
         Ok(_) => {
-            let dest_addr = String::from_utf8_lossy(&buffer[..]);
-            dest_connection(dest_addr);
+            let message = String::from_utf8_lossy(&buffer[..]);
+            dest_connection(client_stream, message[1]);
         },
         Err(_) => {
-            println!("An error occured, terminating connection with {}", client_stream.peer_addr().unwrap());
+            println!("An error occurred, terminating connection with {}", client_stream.peer_addr().unwrap());
             stream.shutdown(Shutdown::Both).expect("Shutdown failed...");
         }
     } {}
 }
 
-fn dest_connection(dest_addr: Cow<str>) {
+fn dest_connection(client_stream: TcpStream, dest_addr: &str) {
     if let Ok(dest_stream) = TcpStream::connect(dest_addr) {
         println!("Connected to dest addr {}", dest_addr);
-        handle_dest_connection(dest_stream);
+        handle_dest_connection(client_stream, dest_stream);
     } else {
         println!("Couldn't connect to dest addr {}", dest_addr);
     }
 }
 
-fn handle_dest_connection(mut dest_stream: TcpStream) {
+fn handle_dest_connection(client_stream: TcpStream, mut dest_stream: TcpStream) {
     let mut buffer = [0; 1024];
 
-    //request something from dest_serve
+    //request something from dest_server
 
     'reading_dest: while match dest_stream.read(&mut buffer) {
-
+        Ok(_) => {
+            let message = String::from_utf8_lossy(&buffer[..]);
+        },
+        Err(e) => {
+            println!("Error {} occurred...", e);
+        }
     }
 }
